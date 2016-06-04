d7db6a372e90dd461881e0d0c1a32e5a0185164a
Justin Co <jcoen5209@gmail.com>
2016-06-03 21:06:56 -0400

    Worked on jade syntax and Express logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	request('https://api.twitch.tv/kraken/games/top/?limit=1', function(error, response, body) {
+		if (!error && response.statusCode == 200) {	
+			console.log('inside request');
+			var bodyJson = JSON.parse(body);
+			var topGame = bodyJson.top[0].game.name;
+			console.log(topGame);
+			console.log(typeof topGame);
+			res.render('index', { title: 'Twitch Audio', game: topGame });
+		}
+	});
 });
+
+// router.get('/', function(req, res, next) {
+
+// });   
 
 module.exports = router;
