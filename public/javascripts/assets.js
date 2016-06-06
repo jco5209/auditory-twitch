65b298cf731b7e5a4502398002cbd248ce3375a8
Justin Co <jcoen5209@gmail.com>
2016-06-06 18:50:03 -0400

    Added functionality to Pause/Play & Volume Up/Down

--- public/javascripts/assets.js
+++ public/javascripts/assets.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  console.log('inside assets.js');
+var playerPlay = document.getElementById("play");
+var playerPause = document.getElementById("pause");
+
+var playerUp = document.getElementById("volumeUp");
+var playerDown = document.getElementById("volumeDown");
+
+playerPlay.addEventListener("click", function() {
+	player.play();
+});
+
+playerPause.addEventListener("click", function() {
+	player.pause();
+});
+
+playerUp.addEventListener("click", function() {
+	player.setVolume(player.getVolume() + 0.1);
+});
+
+playerDown.addEventListener("click", function() {
+	player.setVolume(player.getVolume() - 0.1);
 });
